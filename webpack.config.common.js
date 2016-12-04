67baa7bce4c6f27cad0df46ae843c6a4d4cbb949
martin byrne <marb61a@gmail.com>
2016-12-04 10:46:34 +0000

    updating webpack config file

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -7,5 +7,34 @@ module.exports = {
     
     resolve : {
         extensions: ['.js', '.ts']
-    }
+    },
+    
+    module : {
+        loaders : [
+            {
+                test : /\.ts$/,
+                loaders : [
+                    'awesome-typescript-loader',
+                    'angular2-template-loader',
+                    'angular2-router-loader'    
+                ]
+            },
+            {
+                test : /\.html$/,
+                loader : 'html'
+            },
+            {
+                test : /\.css$/,
+                loader : 'raw'
+            }
+        ]
+    },
+    
+    plugins : [
+        new webpack.ContextReplacementPlugin(
+            // Path separators for bot *nix and Windows platforms
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            './src' // Location of src
+        )    
+    ]
 };
