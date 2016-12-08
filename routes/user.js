5f93bef705e1285e1cc61b6dea9c133bb5c9c262
martin byrne <marb61a@gmail.com>
2016-12-08 10:35:46 +0000

    added user signin route

--- routes/user.js
+++ routes/user.js
@@ -26,5 +26,38 @@ router.post('/', function(req, res, next){
     });
 });
 
+router.post('/signin', function(req, res, next){
+    User.findOne({email : req.body.email}, function(err, user){
+        if(err){
+            return res.status(500).json({
+                title : 'Error has occurred',
+                error : err
+            });    
+        }
+        
+        if(!user){
+            return res.status(401).json({
+                title : 'Login Failed',
+                error : {message : 'Invalid Login Credentials'}
+            });
+        }
+        
+        if(!bcrypt.compareSync(req.body.password, user.password)){
+            return res.status(401).json({
+                title : 'Login Failed',
+                error : {message : 'Invalid Login Credentials'}
+            });    
+        }
+        
+        var token = jwt.sign({user : user}, 'secret', {expiresIn : 7200});
+        
+        res.status(200).json({
+            message : 'Successfully Logged In',
+            token : token,
+            userId : user._id
+        });
+    });
+});
+
 
 module.exports = router;
