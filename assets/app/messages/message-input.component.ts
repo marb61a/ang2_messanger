e2f76f935b72ee9a0d3a7024e2a7bb5628909372
martin byrne <marb61a@gmail.com>
2016-12-14 11:10:18 +0000

    added message-input component html

---
+++ assets/app/messages/message-input.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from "@angular/core";
+import { NgForm } from "@angular/forms";
+
+import { MessageService } from "./message.service";
+import { Message } from "./message.model";
+
+@Component({
+    selector: 'app-message-input',
+    templateUrl: './message-input.component.html'
+})
