09773434f0f9cae880a4be9b04f2dcef16a871fd
martin byrne <marb61a@gmail.com>
2016-12-14 11:24:26 +0000

    updated message-input component ts

--- assets/app/messages/message-input.component.ts
+++ assets/app/messages/message-input.component.ts
@@ -8,3 +8,41 @@ import { Message } from "./message.model";
     selector: 'app-message-input',
     templateUrl: './message-input.component.html'
 })
+
+export class MessageInputComponent implements OnInit {
+    message : Message
+    
+    constructor(private messageService : MessageService){
+    }
+    
+    onSubmit(form : ngForm){
+        if(this.message){
+            // Edit
+            this.message.content = form.value.content;
+            this.messageService.updateMessage(this.message)
+                .subscribe(
+                    result => console.log(result);
+                );
+            this.message = null;
+        } else {
+            // Create
+            const message = new Message(form.value.content, 'Martin');
+            this.messageService.addMessage(message)
+                .subscribe(
+                    data => console.log(data);    
+                );
+        }
+        form.resetForm();
+    }
+    
+    onClear(form: NgForm) {
+        this.message = null;
+        form.resetForm();
+    }
+
+    ngOnInit() {
+        this.messageService.messageIsEdit.subscribe(
+            (message: Message) => this.message = message
+        );
+    }
+}
