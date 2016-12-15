b575d34489a2500434393b0d6274b0fc2348baa2
martin byrne <marb61a@gmail.com>
2016-12-15 09:44:33 +0000

    updated message service with addmessage function

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -14,7 +14,30 @@ export class MessageService {
     constructor(private http: Http, private errorService: ErrorService) {
     }
     
-    addMessage(){
+    addMessage(message : Message){
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type' : 'application/json'});
+        const token = localStorage.getItem('token') ? '?token=' + localStorage.getItem('token') : '';
         
+        return this.http.post('http://localhost:3000/message' + token, body, {headers: headers})
+            .map((response : Response) => {
+                const result = response.json();
+                const message = new Message(
+                    result.obj.content,
+                    result.obj.user.firstName,
+                    result.obj._id,
+                    result.obj.user._id
+                );
+                this.messages.push(message);
+                return message;
+            })
+            .catch((error : Response) => {
+                this.errorService.handleError(error.json());
+                return Observable.throw(error.json());
+            });
+    }
+    
+    editMessage(message : Message){
+        this.messageIsEdit.emit(message);
     }
 }
