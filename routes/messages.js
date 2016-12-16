3071be1ac3eeae4fd38fba98b82ab240a3123c37
martin byrne <marb61a@gmail.com>
2016-12-16 13:59:40 +0000

    updated message routes with post and use functions

--- routes/messages.js
+++ routes/messages.js
@@ -22,4 +22,48 @@ router.get('/', function(req, res, next) {
         });
 });
 
+router.use('/', function(req, res, next){
+    jwt.verify(req.query.token, 'secret', function(err, decoded){
+        if(err){
+            return res.status(401).json({
+                title: 'Not Authenticated',
+                error: err
+            });
+        }
+        next();
+    });
+});
+
+router.post('/', function(req, res, next){
+    var decoded = jwt.decode(req.query.token);
+    User.findById(decoded.user._id, function(err, user){
+        if(err){
+            return res.status(500).json({
+                title: 'An error has occurred',
+                error: err
+            });    
+        }
+        var message = new Message({
+            content : req.body.content,
+            user : user
+        });
+        message.save(function(err, result){
+            if(err){
+                return res.status(500).json({
+                    title: 'An error has occurred',
+                    error: err
+                });    
+            }
+            user.messages.push(result);
+            user.save();
+            return res.status(201).json({
+                title: 'Saved Message',
+                obj : result
+            }); 
+        });
+    });
+});
+
+
+
 module.exports = router;
