4e45860a2c2f2bb63d483702b45fb42c66a87f66
martin byrne <marb61a@gmail.com>
2016-12-18 11:46:56 +0000

    added delete function to messages routes

--- routes/messages.js
+++ routes/messages.js
@@ -26,8 +26,8 @@ router.use('/', function(req, res, next){
     jwt.verify(req.query.token, 'secret', function(err, decoded){
         if(err){
             return res.status(401).json({
-                title: 'Not Authenticated',
-                error: err
+                title : 'Not Authenticated',
+                error : err
             });
         }
         next();
@@ -102,5 +102,42 @@ router.patch('/:id', function(req, res, next){
     });
 });
 
+
+router.delete('/:id', function(req, res, next){
+    var decoded = jwt.decode(req.query.token);
+    Message.findById(req.params.id, function(err, message){
+        if(err){
+            return res.status(500).json({
+                title : 'An error has occurred',
+                error : err
+            });    
+        }
+        if(!message){
+            return res.status(500).json({
+                title : 'No Message Found',
+                error : {message : 'Message not found'}
+            });    
+        }
+        if(message.user != decoded.user._id){
+            return res.status(401).json({
+                title : 'Not Authenticated',
+                error : {message : 'Users do not match'}
+            });    
+        }
+        message.content = req.body.content;
+        message.save(function(err, result){
+            if(err){
+                return res.status(500).json({
+                    title : 'An error has occurred',
+                    error : err
+                });   
+            }
+            res.status(200).json({
+                title: 'Deleted Message',
+                object : result
+            });
+        });
+    });
+});
 
 module.exports = router;
